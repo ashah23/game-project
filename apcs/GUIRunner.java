1dba93eceeb59afd95c3a836caf4b09ecf03d635
ashah23 <arpan.shah0023@gmail.com>
2015-04-29 10:08:38 -0400

    I added mancala things

---
+++ apcs/GUIRunner.java
@@ -0,0 +1,21 @@
+/*Name:	
+ *Date:
+ *Period:
+ *Teacher:
+ *Description:
+ */
+package apcs;
+
+
+public class GUIRunner
+{
+
+    public static void main(String[] args)
+    {
+        Mancala board=new Mancala();
+        MancalaGameGUI gui= new MancalaGameGUI(board);
+        gui.displaygame();
+
+    }
+
+}
---
+++ apcs/MancalaGameGUI.java
@@ -0,0 +1,37 @@
+/*Name:	
+ *Date:
+ *Period:
+ *Teacher:
+ *Description:
+ */
+package apcs;
+import java.awt.Point;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MancalaGameGUI extends JFrame implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
